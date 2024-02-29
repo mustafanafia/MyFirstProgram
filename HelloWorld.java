71e9b57b4e83c231089eb7bf16f4cf271c01fd84
mustafanafia <55895079+mustafanafia@users.noreply.github.com>
2024-02-29 13:59:54 -0500

    Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+//  ***************************************************************************
+//  Program: HelloWorld
+//  Date: 8/16/2018
+//  IDE use: BlueJ
+//  Purpose: To help students verify a proper installation of Java and IDE 
+//  ***************************************************************************
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        /*----------------------------------------------
+                    Displays a Hello, World! message
+                   
+        ------------------------------------------------ */
+        System.out.println("Hello, World!");
+        System.out.println("My name is Mustafa!");
+        System.out.println();
+        System.out.println("I will be your virtual assistant.");
+        System.out.println("It is a pleasure to welcome you in my world.");
+    }
+}
+    
